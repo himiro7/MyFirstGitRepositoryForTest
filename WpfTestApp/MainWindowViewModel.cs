86545485fb703c957fb608a2b9d53ae417e0ccca
himiro7 <hirimo7@gmail.com>
2014-04-12 21:19:52 +0900

    Add base files

---
+++ WpfTestApp/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+using MicroMvvm;
+
+namespace WpfTestApp
+{
+  public class MainWindowViewModel : ObservableObject
+  {
+    public MainWindowViewModel()
+    {
+      Texts = new List<TextSetModel>() {
+            new TextSetModel() { text01="AAAA", text02="111"},
+            new TextSetModel() { text01="BBBB", text02="222"}
+          };
+    }
+
+    public List<TextSetModel> Texts { get; set; }
+
+  }
+}
---
+++ WpfTestApp/TextSetModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfTestApp
+{
+  public class TextSetModel
+  {
+    public string text01 { get; set; }
+    public string text02 { get; set; }
+  }
+}
